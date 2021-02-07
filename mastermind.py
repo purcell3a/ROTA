c02c0214127e360173c826e050c7d23b87b8da9c
Angela Purcell <kalmpurcell@gmail.com>
2021-02-07 05:08:07 +0000

    new reset

--- mastermind.py
+++ mastermind.py
@@ -23,16 +23,16 @@ def next_level(level,headers):
   # ************************************************
   for num in numbers:
       response = guess([num] + [8,9,10],headers)
-      if response == [1,1]:
+      if response == [1, 1]:
         final_guess.append(num)
         numbers.remove(num)
         found_first(final_guess,correct_numbers,numbers,headers)
         break
         print('BREAK NOT WORKING')
-      if response == [1,0]:
+      if response == [1, 0]:
           correct_numbers.append(num)
           numbers.remove(num)
-      if response == [0,0]:
+      if response == [0, 0]:
           numbers.remove(num)
   # ************************************************
 
@@ -61,29 +61,22 @@ def found_first(final_guess,correct_numbers,numbers,headers):
 
 def now_sort(final_guess,correct_numbers,numbers,headers):
   print('sorting')
-  # for num in numbers:
-  #   response = guess([num]+[8,9] + correct_numbers,headers)
-  #   if response == [2,2]:
-  #     final_guess.append(num)
-  #     final_guess.append(correct_numbers)
-  #   elif response == [2,1]:
-  #     response = guess([num]+ correct_numbers +[8,9],headers)
 
 def found_second(final_guess,correct_numbers,numbers,headers):
   print('FOUND SECOND')
   while len(correct_numbers) < 2:
     for num in numbers:
       response = guess(final_guess + [num] + [8],headers)
-      if response == [3,3]:
+      if response == [3, 3]:
           final_guess.append(num)
           numbers.remove(num)
           print('THIRD',final_guess,'correct',correct_numbers,'nums',numbers)
           found_third(final_guess,correct_numbers,numbers,headers)
           break
-      if response == [3,2]:
+      if response == [3, 2]:
           correct_numbers.append(num)
           numbers.remove(num)
-      if response[2,2]:
+      if response[2 ,2]:
           numbers.remove(num)
   print('SECOND',final_guess,'correct',correct_numbers,'nums',numbers)
   third_step(final_guess,correct_numbers,numbers)
@@ -97,13 +90,13 @@ def found_third(final_guess,correct_numbers,numbers,headers):
   else:
     for num in numbers:
       response = guess(final_guess + [num],headers)
-      if response == [4,4]:
+      if response == [4, 4]:
           final_guess.append(num)
           numbers.remove(num)
           print(response)
           next_level(2,headers)
           break
-      if response == [3,3]:
+      if response == [3, 3]:
           numbers.remove(num)
 
 def third_step(final_guess,correct_numbers,numbers):
@@ -130,24 +123,3 @@ def right_numbers(guess,headers):
 # restart()
 
 start_game()
-
-
-
-# import requests, json, sys
-# if sys.version_info < (3,0):
-#   sys.exit('Python version < 3.0 does not support modern TLS versions. You will have trouble connecting to our API using Python 2.X.')
-# email = 'other@lipade.com' # Change this!
-# r = requests.post('https://mastermind.praetorian.com/api-auth-token/', data={'email':email})
-# r.json()
-# # > {'Auth-Token': 'AUTH_TOKEN'}
-# headers = r.json()
-# headers['Content-Type'] = 'application/json'
-
-# # Interacting with the game
-# r = requests.get('https://mastermind.praetorian.com/level/1/', headers=headers)
-# r.json()
-# # > {'numGladiators': 4, 'numGuesses': 8, 'numRounds': 1, 'numWeapons': 6}
-# r = requests.post('https://mastermind.praetorian.com/level/1/', data=json.dumps({'guess':[1,2,3,4]}), headers=headers)
-# re = r.json()
-# print (re)
-# # > {'response': [2, 1]}
