4ed0e27da39a6f10075f822505351c8ebd54680e
Angela Purcell <kalmpurcell@gmail.com>
2021-02-07 04:56:26 +0000

    hard reset point

--- mastermind.py
+++ mastermind.py
@@ -5,15 +5,15 @@ if sys.version_info < (3,0):
   sys.exit('Python version < 3.0 does not support modern TLS versions. You will have trouble connecting to our API using Python 2.X.')
 
 def start_game():
-  r = requests.post('https://mastermind.praetorian.com/api-auth-token/', data={'email':'other@lipade.com'})
+  r = requests.post('https://mastermind.praetorian.com/api-auth-token/', data={'email':'othlker@lipade.com'})
   r.json()
   headers = r.json()
   headers['Content-Type'] = 'application/json'
-  next_level(headers)
+  next_level(1,headers)
 
 # Interacting with the game
-def next_level(headers):
-  r = requests.get('https://mastermind.praetorian.com/level/1/', headers=headers)
+def next_level(level,headers):
+  r = requests.get(f'https://mastermind.praetorian.com/level/{level}/', headers=headers)
   r.json()
 
   final_guess = []
@@ -21,58 +21,53 @@ def next_level(headers):
   numbers = [0,1,2,3,4,5]
 
   # ************************************************
-  for num in numbers[0:3]:
+  for num in numbers:
       response = guess([num] + [8,9,10],headers)
       if response == [1,1]:
         final_guess.append(num)
         numbers.remove(num)
         found_first(final_guess,correct_numbers,numbers,headers)
         break
+        print('BREAK NOT WORKING')
       if response == [1,0]:
           correct_numbers.append(num)
           numbers.remove(num)
       if response == [0,0]:
           numbers.remove(num)
-  no_first(final_guess,correct_numbers,numbers,headers)
   # ************************************************
 
 
 
 def found_first(final_guess,correct_numbers,numbers,headers):
   print('FOUND FIRST')
-  while len(correct_numbers) < 3:
-    for index,num in enumerate(numbers):
-      response = guess(final_guess + [num] + [8,9],headers)
-      if response == [2,2]:
+  for index,num in enumerate(numbers):
+    response = guess(final_guess + [num] + [8,9],headers)
+    if response == [2, 2]:
         final_guess.append(num)
         numbers.remove(num)
         print('FIRST',final_guess,'correct',correct_numbers,'nums',numbers)
         found_second(final_guess,correct_numbers,numbers,headers)
         break
-      if response == [2,1]:
+    if response == [2, 1]:
           correct_numbers.append(num)
           numbers.remove(num)
-      if response == [1,1]:
+    if response == [1, 1]:
           correct_numbers.append(num)
           numbers.remove(num)
-          # WE NEED A FUNCTION FOR WHEN WE DON'T FIND THE SECOND
-
-
-def no_first(final_guess,correct_numbers,numbers,headers):
-  print('DIDNT FIND FIRST')
-  if len(correct_numbers) == 1:
-    now_sort(final_guess,correct_numbers,numbers,headers)
-
+          print('NOWSORTING')
+          print(final_guess,correct_numbers,numbers)
+          now_sort(final_guess,correct_numbers,numbers,headers)
 
 
 def now_sort(final_guess,correct_numbers,numbers,headers):
-  for num in numbers:
-    response = guess([num]+[8,9] + correct_numbers,headers)
-    if response == [2,2]:
-      final_guess.append(num)
-      final_guess.append(correct_numbers)
-    elif response == [2,1]:
-      response = guess([num]+ correct_numbers +[8,9],headers)
+  print('sorting')
+  # for num in numbers:
+  #   response = guess([num]+[8,9] + correct_numbers,headers)
+  #   if response == [2,2]:
+  #     final_guess.append(num)
+  #     final_guess.append(correct_numbers)
+  #   elif response == [2,1]:
+  #     response = guess([num]+ correct_numbers +[8,9],headers)
 
 def found_second(final_guess,correct_numbers,numbers,headers):
   print('FOUND SECOND')
@@ -97,15 +92,16 @@ def found_third(final_guess,correct_numbers,numbers,headers):
   print('FOUND THIRD')
   if correct_numbers != []:
     response = guess(final_guess + correct_numbers,headers)
-    print('DID WE DO IT?!', response)
+    print(response)
+    next_level(2,headers)
   else:
     for num in numbers:
       response = guess(final_guess + [num],headers)
       if response == [4,4]:
           final_guess.append(num)
           numbers.remove(num)
-          print('did we do it????',final_guess,'correct',correct_numbers,'nums',numbers)
-          # START A NEW LEVEL HERE
+          print(response)
+          next_level(2,headers)
           break
       if response == [3,3]:
           numbers.remove(num)
